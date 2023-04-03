bf0324c7f63bcc03d64e5dae2d7162a7aa14a8d3
kvgagan98 <kvgagan98@gmail.com>
2023-04-02 19:48:33 -0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#/!/bin/bash
+BUILD_DIR=build
+if [ ! -d "$BUILD_DIR" ];
+then
+    echo "Creating $BUILD_DIR directory..."
+    mkdir build
+fi
+cd build
+export PICO_SDK_PATH=pico-sdk
+cmake -DPICO_BOARD=pico_w -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+make
